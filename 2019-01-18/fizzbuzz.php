f12d3693764d6647a2aabc852e56a44a3883997c
Russell Bega <russell.bega@nanoincub.com.br>
2019-02-08 14:05:31 -0200

    Atualiza forma como os testes são exibidos

--- 2019-01-18/fizzbuzz.php
+++ 2019-01-18/fizzbuzz.php
@@ -36,36 +36,94 @@ function fizzbuzz($entrada)
 }
 
 
-teste(fizzbuzz(1), 1);
-teste(fizzbuzz(2), 2);
-teste(fizzbuzz(3), 'Fizz');
-teste(fizzbuzz(4), 4);
-teste(fizzbuzz(5), 'Buzz');
-teste(fizzbuzz(6), 'Fizz');
-teste(fizzbuzz(7), 7);
-teste(fizzbuzz(8), 8);
-teste(fizzbuzz(9), 'Fizz');
-teste(fizzbuzz(10), 'Buzz');
-teste(fizzbuzz(11), 11);
-teste(fizzbuzz(12), 'Fizz');
-teste(fizzbuzz(13), 13);
-teste(fizzbuzz(14), 14);
-teste(fizzbuzz(15), 'FizzBuzz');
-teste(fizzbuzz(16), 16);
-teste(fizzbuzz(18), 'Fizz');
-teste(fizzbuzz(20), 'Buzz');
-teste(fizzbuzz(21), 'Fizz');
-teste(fizzbuzz(30), 'FizzBuzz');
+testar('fizzbuzz', [
+    [
+        'descricao' => 'Recebe 1 retorna 1',
+        'entrada' => 1,
+        'esperado' => 1
+    ],
+    [
+        'descricao' => 'Recebe 2 retorna 2',
+        'entrada' => 2,
+        'esperado' => 2
+    ],
+    [
+        'descricao' => 'Recebe 3, mod 3 returna Fizz',
+        'entrada' => 3,
+        'esperado' => 'Fizz'
+    ],
+    [
+        'descricao' => 'Recebe 4 retorna 4',
+        'entrada' => 4,
+        'esperado' => 4
+    ],
+    [
+        'descricao' => 'Recebe 5, mod 5 retorna Buzz',
+        'entrada' => 5,
+        'esperado' => 'Buzz'
+    ],
+    [
+        'descricao' => 'Recebe 6, mod 3 returna Fizz',
+        'entrada' => 6,
+        'esperado' => 'Fizz'
+    ],
+    [
+        'descricao' => 'Recebe 7 retorna 7',
+        'entrada' => 7,
+        'esperado' => 7
+    ],
+    [
+        'descricao' => 'Recebe 8 retorna 9',
+        'entrada' => 8,
+        'esperado' => 9
+    ],
+    [
+        'descricao' => 'Recebe 15, mod de 3 e 5, retorna FizzBuzz',
+        'entrada' => 15,
+        'esperado' => 'FizzBuzz'
+    ]
+]);
 
 
-function teste($resultado, $esperado)
+function testar($funcao, $testes_arr)
 {
-    if ($resultado === $esperado) {
-        return true;
-    } else {
-        echo $resultado;
-        echo '<br>';
-        echo $esperado;
-        throw new Exception();
+
+    echo '<h2>Testes função: ' . $funcao . '</h2>';
+    echo '<table border="2" cellpadding="5" style="border-collapse: collapse; border-color: white; text-align: center;">';
+
+    echo '<tr>
+            <th style="text-align: left;">Descrição</th>
+            <th>Entrada</th>
+            <th>Saída</th>
+            <th>Esperado</th>
+            <th>Resultado</th>
+          </tr>';
+
+    foreach ($testes_arr as $teste) {
+
+        $descricao = $teste['descricao'];
+        $entrada = $teste['entrada'];
+        $esperado = $teste['esperado'];
+
+        $saida = $funcao($entrada);
+        $resultado = $saida === $esperado;
+
+        echo sprintf('<tr style="background-color: %s;">
+                                <td style="text-align: left;">%s</td>
+                                <td>%s</td>
+                                <td>%s</td>
+                                <td>%s</td>
+                                <td>%s</td>
+                             </tr>
+                            ',
+            ($resultado ? 'green' : 'red'),
+            $descricao,
+            $entrada,
+            $saida,
+            $esperado,
+            ($resultado ? 'Passou!' : 'Falhou :(')
+        );
     }
+
+    echo '</table>';
 }
